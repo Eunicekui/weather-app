730721d8cf6200c59c2b778d1afa65e1bfcf9436
Eunicekui <kamaueunice384@gmail.com>
2024-05-31 14:52:45 +0300

    API key

--- src/index.js
+++ src/index.js
@@ -40,7 +40,7 @@ function formatDate(date) {
 }
 
 function searchCity(city) {
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiKey = "fd6ct038d9a222862b05c2bc4o0a3425";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
   axios.get(apiUrl).then(refreshWeather);
 }
